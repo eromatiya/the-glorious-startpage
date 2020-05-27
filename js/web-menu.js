09c5de95b233578ac13d33fd9a6a2e2b0939f8af
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-27 19:57:24 +0800

    add more weblinks

--- js/web-menu.js
+++ js/web-menu.js
@@ -3,14 +3,23 @@ var webMenuSearchBox = document.getElementById("webMenuSearchBox");
 
 // Data
 var webSites = [
-{ site: 'Reddit', icon: 'reddit', url: 'https://redit.com'},
-{ site: 'Github', icon: 'github', url: 'https://github.com'},
-{ site: 'Facebook', icon: 'facebook', url: 'https://facebook.com'},
-{ site: 'Gmail', icon: 'gmail', url: 'https://mail.google.com'},
-{ site: 'Youtube', icon: 'youtube', url: 'https://youtube.com'},
-{ site: 'GDrive', icon: 'gdrive', url: 'https://drive.google.com'},
-{ site: 'Twitter', icon: 'twitter', url: 'https://twitter.com'},
-{ site: 'Instagram', icon: 'instagram', url: 'https://instagram.com'}
+{ site: 'Reddit', icon: 'reddit', url: 'https://redit.com/'},
+{ site: 'Github', icon: 'github', url: 'https://github.com/'},
+{ site: 'Facebook', icon: 'facebook', url: 'https://facebook.com/'},
+{ site: 'Gmail', icon: 'gmail', url: 'https://mail.google.com/'},
+{ site: 'Youtube', icon: 'youtube', url: 'https://youtube.com/'},
+{ site: 'GDrive', icon: 'gdrive', url: 'https://drive.google.com/'},
+{ site: 'Twitter', icon: 'twitter', url: 'https://twitter.com/'},
+{ site: 'Instagram', icon: 'instagram', url: 'https://instagram.com/'},
+
+{ site: 'Bitbucket', icon: 'bitbucket', url: 'https://bitbucket.org/'},
+{ site: 'Gitlab', icon: 'gitlab', url: 'https://gitlab.com/'},
+{ site: 'Deviantart', icon: 'deviantart', url: 'https://deviantart.com/'},
+{ site: 'Duckduckgo', icon: 'duckduckgo', url: 'https://duckduckgo.com/'},
+{ site: 'Ecosia', icon: 'ecosia', url: 'https://ecosia.org/'},
+{ site: 'Google', icon: 'google', url: 'https://google.com/'},
+{ site: 'Wikipedia', icon: 'wikipedia', url: 'https://wikipedia.org/'},
+{ site: 'Unsplash', icon: 'unsplash', url: 'https://unsplash.com/'},
 ];
 
 // Generate a list
@@ -63,7 +72,7 @@ for (i = 0; i < (webSites.length); i++) {
 
 
 // Search through the list
-webMenuSearchBox.onkeyup = function() {
+webMenuSearchBox.onkeyup = function(event) {
 	// Declare variables
 	var input, filter, ul, li, a, i, txtValue;
 	input = webMenuSearchBox;
@@ -71,15 +80,19 @@ webMenuSearchBox.onkeyup = function() {
 	ul = webMenuList;
 	li = ul.getElementsByTagName('li');
 
+	if (event.keyCode === 13) {
+		
+	};
+
 	// Loop through all list items, and hide those who don't match the search query
 	for (i = 0; i < li.length; i++) {
 		a = li[i].getElementsByClassName("webItem")[0];
 		txtValue = a.innerHTML || a.textContent || a.innerText;
 		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-			// Selected tile
+			// Filtered tile/s
 			li[i].style.display = "";
 		} else {
-			// Hidden tile
+			// Hidden tile/s
 			li[i].style.display = "none";
 		}
 	}
