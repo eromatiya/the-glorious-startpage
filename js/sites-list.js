6b5971117de1ccb558cd74cf0a9d0774bf894596
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-29 06:59:26 +0800

    separate websites list js for dock and webmenu

---
+++ js/sites-list.js
@@ -0,0 +1,47 @@
+// You can easily add data here and it will be auto generated
+
+// Web menu
+// A list of websites that will be generated and put on the web menu
+var webSites = [
+	{ site: 'Reddit', icon: 'reddit', url: 'https://reddit.com/'},
+	{ site: 'Github', icon: 'github', url: 'https://github.com/'},
+	{ site: 'Facebook', icon: 'facebook', url: 'https://facebook.com/'},
+	{ site: 'Gmail', icon: 'gmail', url: 'https://mail.google.com/'},
+	{ site: 'Youtube', icon: 'youtube', url: 'https://youtube.com/'},
+	{ site: 'GDrive', icon: 'gdrive', url: 'https://drive.google.com/'},
+	{ site: 'Twitter', icon: 'twitter', url: 'https://twitter.com/'},
+	{ site: 'Instagram', icon: 'instagram', url: 'https://instagram.com/'},
+	{ site: 'Bitbucket', icon: 'bitbucket', url: 'https://bitbucket.org/'},
+	{ site: 'Gitlab', icon: 'gitlab', url: 'https://gitlab.com/'},
+	{ site: 'Deviantart', icon: 'deviantart', url: 'https://deviantart.com/'},
+	{ site: 'Duckduckgo', icon: 'duckduckgo', url: 'https://duckduckgo.com/'},
+	{ site: 'Ecosia', icon: 'ecosia', url: 'https://ecosia.org/'},
+	{ site: 'Google', icon: 'google', url: 'https://google.com/'},
+	{ site: 'Wikipedia', icon: 'wikipedia', url: 'https://wikipedia.org/'},
+	{ site: 'Unsplash', icon: 'unsplash', url: 'https://unsplash.com/'},
+	{ site: 'Twitch', icon: 'twitch', url: 'https://twitch.tv/'},
+	{ site: 'Yahoo', icon: 'yahoo', url: 'https://mail.yahoo.com/'},
+	{ site: 'Pornhub', icon: 'pornhub', url: 'https://pornhub.com/'},
+	{ site: 'Material.io', icon: 'materialio', url: 'https://material.io/'},
+	{ site: 'Netflix', icon: 'netflix', url: 'https://netflix.com/'},
+	{ site: 'Office 365', icon: 'office365', url: 'https://office.com/'},
+	{ site: '4chan', icon: '4chan', url: 'https://4chan.org/'},
+	{ site: 'Discord', icon: 'discord', url: 'https://discord.com/'},
+	{ site: 'Spotify', icon: 'spotify', url: 'https://spotify.com/'},
+	{ site: 'Ebay', icon: 'ebay', url: 'https://ebay.com/'}
+];
+
+
+// Dock
+// A list of websites that will be generated and put on the dock
+let dockSites = [
+	{ site: 'Reddit', icon: 'reddit', url: 'https://reddit.com/'},
+	{ site: 'Github', icon: 'github', url: 'https://github.com/'},
+	{ site: 'Facebook', icon: 'facebook', url: 'https://facebook.com/'},
+	{ site: 'Gmail', icon: 'gmail', url: 'https://mail.google.com/'},
+	{ site: 'Youtube', icon: 'youtube', url: 'https://youtube.com/'},
+	{ site: 'GDrive', icon: 'gdrive', url: 'https://drive.google.com/'},
+	{ site: 'Twitter', icon: 'twitter', url: 'https://twitter.com/'},
+	{ site: 'Unsplash', icon: 'unsplash', url: 'https://unsplash.com/'},
+	{ site: 'Material.io', icon: 'materialio', url: 'https://material.io/'}
+];
--- js/web-menu.js
+++ js/web-menu.js
@@ -4,36 +4,6 @@ var webMenuSearchBox = document.getElementById("webMenuSearchBox");
 
 let webMenuVisible = false;
 
-// Data
-var webSites = [
-{ site: 'Reddit', icon: 'reddit', url: 'https://reddit.com/'},
-{ site: 'Github', icon: 'github', url: 'https://github.com/'},
-{ site: 'Facebook', icon: 'facebook', url: 'https://facebook.com/'},
-{ site: 'Gmail', icon: 'gmail', url: 'https://mail.google.com/'},
-{ site: 'Youtube', icon: 'youtube', url: 'https://youtube.com/'},
-{ site: 'GDrive', icon: 'gdrive', url: 'https://drive.google.com/'},
-{ site: 'Twitter', icon: 'twitter', url: 'https://twitter.com/'},
-{ site: 'Instagram', icon: 'instagram', url: 'https://instagram.com/'},
-{ site: 'Bitbucket', icon: 'bitbucket', url: 'https://bitbucket.org/'},
-{ site: 'Gitlab', icon: 'gitlab', url: 'https://gitlab.com/'},
-{ site: 'Deviantart', icon: 'deviantart', url: 'https://deviantart.com/'},
-{ site: 'Duckduckgo', icon: 'duckduckgo', url: 'https://duckduckgo.com/'},
-{ site: 'Ecosia', icon: 'ecosia', url: 'https://ecosia.org/'},
-{ site: 'Google', icon: 'google', url: 'https://google.com/'},
-{ site: 'Wikipedia', icon: 'wikipedia', url: 'https://wikipedia.org/'},
-{ site: 'Unsplash', icon: 'unsplash', url: 'https://unsplash.com/'},
-{ site: 'Twitch', icon: 'twitch', url: 'https://twitch.tv/'},
-{ site: 'Yahoo', icon: 'yahoo', url: 'https://mail.yahoo.com/'},
-{ site: 'Pornhub', icon: 'pornhub', url: 'https://pornhub.com/'},
-{ site: 'Material.io', icon: 'materialio', url: 'https://material.io/'},
-{ site: 'Netflix', icon: 'netflix', url: 'https://netflix.com/'},
-{ site: 'Office 365', icon: 'office365', url: 'https://office.com/'},
-{ site: '4chan', icon: '4chan', url: 'https://4chan.org/'},
-{ site: 'Discord', icon: 'discord', url: 'https://discord.com/'},
-{ site: 'Spotify', icon: 'spotify', url: 'https://spotify.com/'},
-{ site: 'Ebay', icon: 'ebay', url: 'https://ebay.com/'}
-];
-
 // Create mouse event for passed div
 function addMouseUpEvent(li, url) {
 	li.onmouseup = function() {
