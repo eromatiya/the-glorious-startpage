ba56974c9de1059346c66acb2239a5393b425520
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-28 11:40:43 +0800

    clean some animation codes and onkeypress script

--- js/animate-dashboard.js
+++ js/animate-dashboard.js
@@ -1,7 +1,7 @@
 var dashboard = document.getElementById("rightDashboard");
 var dashboardHider = document.getElementById("overlayHider");
 
-var panelOpen = false;
+var floatPanelVisible = false;
 
 function slideDashboard() {
 
@@ -11,21 +11,21 @@ function slideDashboard() {
 	}
 
 	// Toggle right panel
-	if (!panelOpen) {
+	if (!floatPanelVisible) {
 		dashboard.style.width = "350px"
 	} else {
 		dashboard.style.width = "0"
 	}
 	// Enable overlay
 	dashboardHider.classList.toggle('show');
-	panelOpen = !panelOpen;
+	floatPanelVisible = !floatPanelVisible;
 
 }
 
 
 dashboardHider.addEventListener(
 	"mouseup", function() {
-		if (panelOpen) {
+		if (floatPanelVisible) {
 			slideDashboard();
 		}
 	}
--- js/onkeydown.js
+++ js/onkeydown.js
@@ -1,41 +1,66 @@
+var searchBox = document.getElementById("searchBox");
+
 // Keypress events
-document.onkeydown = function keydown (evt) { 
-    if (!evt) evt = event; 
+document.onkeyup = function keydown (event) { 
 
-    if (searchBoxVisible == false) {
+    if (event.keyCode === 27) {
 
-        // Don't accept this keycodes
-        if (evt.keyCode === 13 || evt.altKey || 
-            evt.ctrlKey || evt.keyCode === 91 || evt.keyCode === 116) { return; };
+        // If searchbox is visible, hide and clear input field
+        if (searchBoxVisible) {
+            // Hide searchbox
+            toggleSearchBox();
+            searchBox.value = '';
+            return;
+        };
 
-        // Check if weather settings is open
-        if (weatherSettingsVisible) { return; };
+        // If dashboard is visible, hide
+        if (floatPanelVisible) {
+            slideDashboard();
+        }
+
+        // Show web menu
+        webMenuToggle();
+        return;
+    }
+
+    // If searchbox is not visible, open and focus
+    if (searchBoxVisible === false) {
 
         // Don't show searchbox when web menu is open
         if (webMenuVisible) {
             return;
         }
 
-    	// Show search box
-	    toggleSearchBox();
-    	searchBox.focus();
+        // Dont accept alt, ctrl,
+        // left/right window key, f5, return
+        if (event.altKey ||
+            event.ctrlKey ||
+            event.keyCode === 91 ||
+            event.keyCode === 92 ||
+            event.keyCode === 116 || 
+            event.keyCode === 13) 
+            { return; };
 
+
+        // Check if weather settings is open
+        if (weatherSettingsVisible) { return; };
+
+        // Open and focus
+        toggleSearchBox();
+        searchBox.focus();
+    
     } else {
-
+        
         // Backspacing while there's no search query will hide searhbox
-        if ((evt.keyCode === 8 || evt.keyCode === 13) && 
+        // Will also hide if you hit enter
+        if ((event.keyCode === 8 || event.keyCode === 13) && 
             searchBox.value < 1) { toggleSearchBox(); return; };
 
-        // Ctrl and space hides searchbox
-        if ((evt.ctrlKey && evt.keyCode === 32) || evt.keyCode === 27 ) {
-        	searchBox.value = '';
-        	toggleSearchBox()
-
-        // Shift and space clears searchbox
-        } else if (evt.shiftKey && evt.keyCode === 32 ) {
-            // Clear searchbox
+        // Clear searchbox
+        if (event.shiftKey && event.keyCode === 32 ) { 
             searchBox.value = '';
-        }
+            return; 
+        };
     }
 }
 
