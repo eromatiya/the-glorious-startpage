c844aa53f1c0e556cca188f94c422995a667acf4
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-30 13:54:38 +0800

    clean clock script

--- js/clock.js
+++ js/clock.js
@@ -1,36 +1,38 @@
+var clock = document.getElementById("clock")
+
 /* Append 0 before time elements if less hour's than 10 */
-function updateTime(k) {
+function appendZero(k) {
 	if (k < 10) {
 		return "0" + k;
-	}
-	else {
+	} else {
 		return k;
 	}
 }
 
 function currentTime() {
-
 	// Date object
 	var date = new Date();
 
 	// Set hour, minute
 	var hour = date.getHours();
 	var min = date.getMinutes();
-	var midday = "AM";
+	var midDay = "AM";
 	
 	// Assigning
-	midday = (hour >= 12) ? "PM" : "AM";
+	midDay = (hour >= 12) ? "PM" : "AM";
 	hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
-	hour = updateTime(hour);
-	min = updateTime(min);
+	hour = appendZero(hour);
+	min = appendZero(min);
 
 	// Update clock id element
-	document.getElementById("clock").innerText = hour + ":" + min + " " + midday;
+	clock.innerText = hour + ":" + min + " " + midDay;
 	
-	// Start timer
+	// Recursion
+	// Call itself if 1 second has passed.
+	// TOKIWO TOMAREEEE!
 	var t = setTimeout(currentTime, 1000);
 }
 
 // Start
-currentTime();
+window.onload = currentTime();
 
