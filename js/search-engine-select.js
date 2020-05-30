b80b42ba99a52eea7e3676a0236dadfbfd098db3
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-31 07:42:42 +0800

    Dashboard (#9)
    * reposition weather and searchengine in dashboard
    
    * reposition weather and searchengine in dashboard
    
    * index.html clean meta tags
    
    * add yahoo to search engines
    
    * add bing to search engines

--- js/search-engine-select.js
+++ js/search-engine-select.js
@@ -23,6 +23,14 @@ function selectQueryString() {
 	} else if (defaultEngine === "ecosia") {
 		searchQueryPrefix = 'https://www.ecosia.org/search?q=';
 		searchBox.placeholder = "Search with Ecosia";
+
+	} else if (defaultEngine === "yahoo") {
+		searchQueryPrefix = 'http://search.yahoo.com/search?p=';
+		searchBox.placeholder = "Search with Yahoo";
+
+	} else if (defaultEngine === "bing") {
+		searchQueryPrefix = 'https://www.bing.com/search?q=';
+		searchBox.placeholder = "Search with Bing";
 	
 	} else {
 		searchQueryPrefix = 'http://www.google.com/search?q=';
