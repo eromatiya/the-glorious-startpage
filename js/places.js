8277f01b4c29e1ba8e3481ae835f19570d1d66b0
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-31 16:24:58 +0800

    show dummy class element

--- js/places.js
+++ js/places.js
@@ -1,17 +1,17 @@
 /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
 
 function myFunction() {
-	document.getElementById("categoryContentDropdown").classList.toggle("dropDownShow");
+	document.getElementById("dummyCategoryContentDropdown").classList.toggle("dropDownShow");
 }
 
-document.getElementById("categoryButton").onclick = function() {
+document.getElementById("dummyCategoryButton").onclick = function() {
 	myFunction();
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
 	if (!event.target.matches('.categoryButton')) {
-		var dropdowns = document.getElementsByClassName("categoryContent");
+		var dropdowns = document.getElementById("dummyCategoryContentDropdown");
 		var i;
 		for (i = 0; i < dropdowns.length; i++) {
 			var openDropdown = dropdowns[i];
