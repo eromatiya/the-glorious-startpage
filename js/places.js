190c753814614b12d4ec60cb80ac6d8bb4ea67f0
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-31 16:08:19 +0800

    create place.js

---
+++ js/places.js
@@ -0,0 +1,23 @@
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+document.getElementById("dropdownbtn").onclick = function() {
+	myFunction();
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
