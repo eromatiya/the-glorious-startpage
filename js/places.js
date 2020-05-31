b69616871be34795a64e6792ed68e9f82d26a3fc
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-31 16:12:45 +0800

    rename id/classes places elements

--- js/places.js
+++ js/places.js
@@ -1,17 +1,17 @@
 /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
 
 function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
+	document.getElementById("categoryContentDropdown").classList.toggle("show");
 }
 
-document.getElementById("dropdownbtn").onclick = function() {
+document.getElementById("categoryButton").onclick = function() {
 	myFunction();
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-		var dropdowns = document.getElementsByClassName("dropdown-content");
+	if (!event.target.matches('.categoryButton')) {
+		var dropdowns = document.getElementsByClassName("categoryContent");
 		var i;
 		for (i = 0; i < dropdowns.length; i++) {
 			var openDropdown = dropdowns[i];
