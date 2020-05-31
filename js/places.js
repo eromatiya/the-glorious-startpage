2a9597bc2511e5ab768c2a708b4ecec69b3c2fd5
Gerome Matilla <gerome.matilla07@gmail.com>
2020-05-31 19:57:39 +0800

    working link places

--- js/places.js
+++ js/places.js
@@ -8,6 +8,13 @@ function toggleCategoryEvent(button, content) {
 	}
 }
 
+// Create mouse event for passed div
+function addMouseUpEvent(li, url) {
+	li.onmouseup = function() {
+		window.location.href = encodeURI(url);
+	}
+}
+
 window.onclick = function(event) {
 	if (!event.target.matches('.categoryButton')) {
 		var dropdowns = document.getElementsByClassName("categoryContent");
@@ -21,17 +28,15 @@ window.onclick = function(event) {
 	}
 }
 
-
 // Populate web menu
 function populatePlaces() {
 
 	// Generate a list
 	for (i = 0; i < (webSites.length); i++) {
 
-
 		var category = webSites[i].category;
 		var site = webSites[i].site;
-
+		var url = webSites[i].url;
 		var categoryIdName = category + 'Category';
 
 		if (document.getElementById(categoryIdName) !== null) {
@@ -42,10 +47,8 @@ function populatePlaces() {
 
 			var content = document.createElement('div');
 			content.innerHTML = site;
+			addMouseUpEvent(content, url);
 			categoryContent.appendChild(content);
-
-
-
 
 		} else {
 
@@ -67,6 +70,7 @@ function populatePlaces() {
 
 			var content = document.createElement('div');
 			content.innerHTML = site;
+			addMouseUpEvent(content, url);
 			categoryContent.appendChild(content);
 
 			toggleCategoryEvent(categoryButton, categoryContent);
@@ -75,54 +79,9 @@ function populatePlaces() {
 			categoryMain.appendChild(categoryButton);
 			categoryMain.appendChild(categoryContent);
 	
-
-
 			places.appendChild(categoryMain);
 
-
-
-
-			// categoryMain.innerHTML = category;
-			// places.appendChild(categoryMain);
-
 		}
-
-
-		
-
-		// var categoryName = webSites[i].category;
-
-		// var categoryIdName = categoryName + 'Category';
-
-		// if (document.getElementById(categoryIdName) !== null) {
-
-		// 	// category already created, just add the item in category
-
-
-		// 	var dummyDiv = document.createElement('option');
-		// 	dummyDiv.innerHTML = categoryName;
-		// 	dummyDiv.style.background = '#ff00ff';
-		// 	document.getElementById(categoryIdName).appendChild(dummyDiv);
-
-		// } else {
-
-		// 	if (categoryName === 'social') {
-		// 		alert(categoryName);
-		// 	}
-
-		// 	// create new category div and add item to it
-
-		// 	var category = document.createElement('select');
-		// 	category.id = categoryIdName;
-		// 	category.innerHTML = categoryName;
-		// 	category.style.background = '#0f0';
-		// 	places.appendChild(category);
-
-		// 	// Insert item in newly created category
-		// 	category.appendChild(document.createElement('option'));
-		// }
-
-
 	}
 
 }
