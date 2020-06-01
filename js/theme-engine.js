7b68e3d90c83a48a49d1ac24e72c9f586bda39e4
Gerome Matilla <gerome.matilla07@gmail.com>
2020-06-01 08:04:34 +0800

    theme-engine.js

--- js/theme-engine.js
+++ js/theme-engine.js
@@ -5,10 +5,17 @@ var blurTextBox = document.getElementById('blurSet');
 function updateTextBoxValues() {
 
 	var baseBG = window.getComputedStyle(document.documentElement).getPropertyValue('--base-bg');
+	var blurStrength = window.getComputedStyle(document.documentElement).getPropertyValue('--blur-strength');
+	
+	// Slice to separate RGB and A
+	var background = baseBG.slice(0, -2);
 	var opacityLevel = baseBG.slice(-2); 
 
-	backgroundTextBox.placeholder = baseBG;
-	blurTextBox.placeholder = window.getComputedStyle(document.documentElement).getPropertyValue('--blur-strength');
+	// Slice to remove px in blur strength
+	var blur = blurStrength.slice(0, -2);
+
+	backgroundTextBox.placeholder = background;
+	blurTextBox.placeholder = blur;
 	opacityTextBox.placeholder = opacityLevel;
 }
 
