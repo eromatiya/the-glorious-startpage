306d7335af8dc3581a5cba97727a26ff587c847a
Gerome Matilla <gerome.matilla07@gmail.com>
2020-06-01 09:32:27 +0800

    add better color checking

--- js/theme-engine.js
+++ js/theme-engine.js
@@ -1,25 +1,31 @@
 var backgroundTextBox = document.getElementById('backgroundSet');
-var backgrouneOpacityTextBox = document.getElementById('backgroundOpacitySet');
+var backgroundOpacityTextBox = document.getElementById('backgroundOpacitySet');
 
 var foregroundTextBox = document.getElementById('foregroundSet');
-var foregrouneOpacityTextBox = document.getElementById('foregroundOpacitySet');
+var foregroundOpacityTextBox = document.getElementById('foregroundOpacitySet');
 
 var blurTextBox = document.getElementById('blurSet');
 
 
-// function checkAlpha(colorStr) {
-// 	if (colorStr.length <= 7) {
-// 		// Add ff to bottom
-// 		// var colorStr 
-// 	}
-// }
+function checkAlpha(colorStr) {
+	if (colorStr.length <= 7) {
+		return colorStr + 'FF';
+	};
+	return colorStr;
+}
 
 // Must be RGBA
 function checkColorValidity(colorStr) {
+	var colorBool = /^#[0-9A-F]{8}$/i.test(colorStr);
 
-	alphaBool = /^#[0-9A-F]{8}$/i.test(colorStr);
-	return alphaBool;
+	if (!colorBool) {
+		var checkedColor = checkAlpha(colorStr);
+		return checkedColor;
+	}
+	return colorStr;
 }
+
+alert(checkColorValidity("#ff00ff20"))
 
 // Get root var value
 // window.getComputedStyle(document.documentElement).getPropertyValue('--color-font-general');
@@ -40,17 +46,26 @@ function checkColorValidity(colorStr) {
 
 function updateTextBoxValues() {
 
+
 	var baseBG = window.getComputedStyle(document.documentElement).getPropertyValue('--base-bg');
+	var baseColor = window.getComputedStyle(document.documentElement).getPropertyValue('--base-color');
 	var blurStrength = window.getComputedStyle(document.documentElement).getPropertyValue('--blur-strength');
 
 	// Remove whitespace
 	baseBG = baseBG.replace(/ /g,'');
+	baseColor = baseColor.replace(/ /g,'');
 	blurStrength = blurStrength.replace(/ /g,'');
-	
+
+	baseBG = baseBG;
+
 	var backgroundColor;
 	var backgroundOpacity;
 
-	if (checkColorValidity(baseBG)) {
+	// var foregroundColor;
+	// var foregroundOpacity;
+
+	// Check BG validity
+	if (baseBG) {
 		// Slice to separate RGB and A of background color
 		
 		// Slice alpha out
@@ -60,8 +75,23 @@ function updateTextBoxValues() {
 		backgroundOpacity = baseBG.slice(-2);
 	}
 
-	backgroundTextBox.placeholder = baseBG;
-	backgrouneOpacityTextBox.placeholder = backgroundOpacity;
+	// Check FG validity
+	// if (checkColorValidity(baseColor)) {
+	// 	// Slice to separate RGB and A of background color
+		
+	// 	// Slice alpha out
+	// 	foregroundColor = baseColor.slice(0, -2);
+
+	// 	// Get alpha
+	// 	foregroundOpacity = baseColor.slice(-2);
+	// }
+
+	// Set placeholders
+	backgroundTextBox.placeholder = backgroundColor;
+	backgroundOpacityTextBox.placeholder = backgroundOpacity;
+
+	// foregroundTextBox.placeholder = foregroundColor;
+	// foregroundOpacityTextBox.placeholder = foregroundOpacity;
 
 
 	// Slice to remove px in blur strength
