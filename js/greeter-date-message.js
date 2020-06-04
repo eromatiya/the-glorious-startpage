a6fce8a7b971396ff1e9b5858750f71651b3a5e5
Gerome Matilla <gerome.matilla07@gmail.com>
2020-06-04 10:47:58 +0800

    right dashboard greeter message cleanup

--- js/greeter-date-message.js
+++ js/greeter-date-message.js
@@ -3,15 +3,7 @@ var dateMessage = document.getElementById("dateMessage");
 
 var date = new Date();
 var hour = date.getHours();
-
-var greeterSuffix = "";
-if (hour >= 6 && hour < 12) {
-	greeterSuffix = "Morning";
-} else if (hour >= 12 && hour < 18) {
-	greeterSuffix = "Afternoon";
-} else {
-	greeterSuffix = "Evening";
-}
+var greeterSuffix;
 
 const monthsArr = [
 	"January",
@@ -38,10 +30,24 @@ const daysArr = [
 	"Saturday"
 ]
 
-function getDayOrdinal(day) {
-	return day + (day > 0 ? ['th', 'st', 'nd', 'rd'][(day > 3 && day < 21) || day % 10 > 3 ? 0 : day % 10] : '');
+const getDayOrdinal = (day) => {
+	return day + (day > 0 ? ['th', 'st', 'nd', 'rd'][(day > 3 && day < 21) ||
+		day % 10 > 3 ? 0 : day % 10] : '');
 }
 
-greeterMessage.innerHTML = "Good" + "<br>" + greeterSuffix + "!";
-dateMessage.innerHTML = "Today is the "  +	getDayOrdinal(date.getDate()) + 
-	" of " + monthsArr[date.getMonth()] + ", and it's " + daysArr[date.getDay()] + ".";
+const updateGreeterDateMessage = () => {	
+	if (hour >= 6 && hour < 12) {
+		greeterSuffix = "Morning";
+	} else if (hour >= 12 && hour < 18) {
+		greeterSuffix = "Afternoon";
+	} else {
+		greeterSuffix = "Evening";
+	}
+
+	greeterMessage.innerHTML = "Good" + "<br>" + greeterSuffix + "!";
+
+	dateMessage.innerHTML = "Today is the "  +	getDayOrdinal(date.getDate()) + 
+							" of " + monthsArr[date.getMonth()] + ", and it's " + daysArr[date.getDay()] + ".";
+}
+
+window.onload = updateGreeterDateMessage();
