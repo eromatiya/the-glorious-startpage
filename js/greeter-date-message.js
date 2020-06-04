5762af432ab936b7fb1c9ce70f68c11e9dceed55
Gerome Matilla <gerome.matilla07@gmail.com>
2020-06-04 10:45:26 +0800

    right dashboard greeter message

---
+++ js/greeter-date-message.js
@@ -0,0 +1,47 @@
+var greeterMessage = document.getElementById("greeterMessage");
+var dateMessage = document.getElementById("dateMessage");
+
+var date = new Date();
+var hour = date.getHours();
+
+var greeterSuffix = "";
+if (hour >= 6 && hour < 12) {
+	greeterSuffix = "Morning";
+} else if (hour >= 12 && hour < 18) {
+	greeterSuffix = "Afternoon";
+} else {
+	greeterSuffix = "Evening";
+}
+
+const monthsArr = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+const daysArr = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+]
+
+function getDayOrdinal(day) {
+	return day + (day > 0 ? ['th', 'st', 'nd', 'rd'][(day > 3 && day < 21) || day % 10 > 3 ? 0 : day % 10] : '');
+}
+
+greeterMessage.innerHTML = "Good" + "<br>" + greeterSuffix + "!";
+dateMessage.innerHTML = "Today is the "  +	getDayOrdinal(date.getDate()) + 
+	" of " + monthsArr[date.getMonth()] + ", and it's " + daysArr[date.getDay()] + ".";
