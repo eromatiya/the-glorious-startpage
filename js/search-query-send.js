f44eb62ec7b66a1bceb5ccc7f455c8b78b0abd47
Gerome Matilla <gerome.matilla07@gmail.com>
2020-06-13 08:02:10 +0800

    allow url search in searchbox

--- js/search-query-send.js
+++ js/search-query-send.js
@@ -1,18 +1,42 @@
 class SearchQuerySend {
 
 	constructor() {
-		// super();
 		this._searchBox = document.querySelector('#searchBox');
 		this._keyUpEvent = this._keyUpEvent.bind(this);
+		
 		this._registerKeyUpEvent();
 	}
 
+	// Check if search query is a valid url
+	_isURL = u => {
+		let dummyInput;
+
+		if (!dummyInput) {
+			dummyInput = document.createElement('input');
+			dummyInput.setAttribute('type', 'url');
+		}
+
+		dummyInput.value = u;
+		return dummyInput.validity.valid;
+	}
+
+	// Search query
 	_sendQuery = () => {
-		// Search
+
+		// If search query is a url, open it
+		if (this._isURL(this._searchBox.value)) {
+			window.location.href = encodeURI(this._searchBox.value);
+			return;
+		}
+
+		// Web search
 		window.location.href = encodeURI(searchEngineSettings.getSearchQueryPrefix() + this._searchBox.value);
 	};
 
 	_keyUpEvent = event => {
+	    // Cancel the default action, if needed
+	    event.preventDefault();
+		
 		if (event.key === 'Tab') return;
 
 		// Number 13 is the "Enter" key on the keyboard
@@ -22,9 +46,6 @@ class SearchQuerySend {
 			if (searchBox.value < 1) {
 				return;
 			}
-
-	    	// Cancel the default action, if needed
-	    	event.preventDefault();
 
 	    	// Search the web
 	    	this._sendQuery()
